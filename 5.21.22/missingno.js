bdcd268310fafdeda02714c9e7bd8f5067826c62
NickRoccodev11 <nchls.rcc@gmail.com>
2022-05-21 12:25:42 -0400

    updated

--- 5.21.22/missingno.js
+++ 5.21.22/missingno.js
@@ -14,3 +14,19 @@ function missingNo(nums) {
         }
     }
 }
+
+
+// Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
+
+// Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+// Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+
+function getMissingElement(superImportantArray) {
+    let sortedArr = superImportantArray.sort((a, b) => a - b);
+    for (i = 0; i <= sortedArr.length; i++) {
+        if (i !== sortedArr[i]) {
+            return i
+        }
+    }
+}
