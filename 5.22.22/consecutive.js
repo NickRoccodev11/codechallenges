420a1780e443d5860cfdb89425adfc4c57f1a71b
NickRoccodev11 <nchls.rcc@gmail.com>
2022-05-22 11:16:58 -0400

    daily challenge

---
+++ 5.22.22/consecutive.js
@@ -0,0 +1,20 @@
+
+// You are given a list of unique integers arr, 
+// and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr,
+//  and return a boolean value (True if a and b are consecutive, False otherwise).
+
+// It is guaranteed that a and b are both present in arr.
+
+
+
+
+
+
+
+function consecutive(arr, a, b) {
+    if (arr.indexOf(a) === arr.indexOf(b) + 1 || arr.indexOf(b) === arr.indexOf(a) + 1) {
+        return true
+    } else {
+        return false
+    }
+}
