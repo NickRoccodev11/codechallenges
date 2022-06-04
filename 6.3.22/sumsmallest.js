c78eb39173851c340199266b0a08dd7353ba31c0
NickRoccodev11 <nchls.rcc@gmail.com>
2022-06-04 10:22:43 -0400

    daily challenge

--- 6.3.22/sumsmallest.js
+++ 6.3.22/sumsmallest.js
@@ -3,6 +3,19 @@
 //  No floats or non-positive integers will be passed.
 
 
-function sumTwoSmallestNumbers(numbers) {  
-    return numbers.sort((a,b)=> a-b)[0]+ numbers.sort((a,b)=> a-b)[1]
+// function sumTwoSmallestNumbers(numbers) {  
+//     return numbers.sort((a,b)=> a-b)[0]+ numbers.sort((a,b)=> a-b)[1]
+//     }
+
+function transpose(matrix) {
+let newArr = []
+for(i=0;i<matrix.length;i++){
+    newArr.push([])
+    for(j=0;j<matrix[i].length;j++){
+        newArr.push(matrix[i][j])
     }
+}
+log(newArr)
+}
+
+transpose([[1,2,3],[4,5,6]])
