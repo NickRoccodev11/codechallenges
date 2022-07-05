996a2f3f2aaa0a880095488b253ca20035b297f6
NickRoccodev11 <nchls.rcc@gmail.com>
2022-07-05 15:35:01 -0400

    daily

---
+++ 7.22/7.5.22/bugs.js
@@ -0,0 +1,21 @@
+// this should tell you if tail is the last letter in the body argument. fix the bugs
+
+
+// function correctTail(bod, tail ;
+  
+//     sub = body.substr(bodylength-(tail.length)
+    
+//     if sub = tail) ;
+//       return true
+//     }
+//     else 
+//       return false
+    
+  
+
+
+
+
+function correctTail(bod, tail) {
+    return bod[bod.length-1] === tail
+  }
