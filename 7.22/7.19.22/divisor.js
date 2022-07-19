ca5c2b3bf1e5fb83208e44a63bcf65237c68ac24
NickRoccodev11 <nchls.rcc@gmail.com>
2022-07-19 11:53:32 -0400

    daily

--- 7.22/7.19.22/divisor.js
+++ 7.22/7.19.22/divisor.js
@@ -5,15 +5,6 @@
 
 // 1. 6 / 2 = 3
 // 2. 3 / 2 = 1 remainder = 1
-// 100 can be divided by 2 six times:
-
-// 1. 100 / 2 = 50
-// 2. 50 / 2 = 25
-// 3. 25 / 2 = 12 remainder 1
-// 4. 12 / 2 = 6
-// 5. 6 / 2 = 3
-// 6. 3 / 2 = 1 remainder 1
-
 
 const divisions = (n, divisor) => {
     if (divisor > n) {
@@ -28,3 +19,5 @@ const divisions = (n, divisor) => {
     }
     return divisionNumber;
 };
+
+
