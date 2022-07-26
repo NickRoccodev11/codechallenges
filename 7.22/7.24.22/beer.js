fad825e6c7c075ec3041bf9dc8b9dbf8b5fabcb5
NickRoccodev11 <nchls.rcc@gmail.com>
2022-07-25 22:04:33 -0400

    ok

--- 7.22/7.24.22/beer.js
+++ 7.22/7.24.22/beer.js
@@ -1,21 +1,23 @@
 // print out the bottles of beer song. No hard coding the whole song!
 //each line is it's own element in an array
 
-var sing = function () {
-    let beer = 99
-    let song = []
-    while (beer > 1) {
-      song.push(`${beer} bottles of beer on the wall, ${beer} bottles of beer.`);
-      beer--;
-      if (beer === 1) {
-        song.push("Take one down and pass it around, one bottle of beer on the wall.")
-        song.push("1 bottle of beer on the wall, 1 bottle of beer.")
-        song.push("Take one down and pass it around, no more bottles of beer on the wall.")
-        song.push("No more bottles of beer on the wall, no more bottles of beer.")
-        song.push("Go to the store and buy some more, 99 bottles of beer on the wall.")
-      } else {
-        song.push(`Take one down and pass it around, ${beer} bottles of beer on the wall.`)
-      }
-    }
-    log(song)
-  };
+// var sing = function () {
+//     let beer = 99
+//     let song = []
+//     while (beer > 1) {
+//       song.push(`${beer} bottles of beer on the wall, ${beer} bottles of beer.`);
+//       beer--;
+//       if (beer === 1) {
+//         song.push("Take one down and pass it around, one bottle of beer on the wall.")
+//         song.push("1 bottle of beer on the wall, 1 bottle of beer.")
+//         song.push("Take one down and pass it around, no more bottles of beer on the wall.")
+//         song.push("No more bottles of beer on the wall, no more bottles of beer.")
+//         song.push("Go to the store and buy some more, 99 bottles of beer on the wall.")
+//       } else {
+//         song.push(`Take one down and pass it around, ${beer} bottles of beer on the wall.`)
+//       }
+//     }
+//     log(song)
+//   };
+
+log("x"==="X")
