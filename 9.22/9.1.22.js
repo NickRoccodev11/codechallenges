3c3d594ecd836fbb6a827ceceb315da5da253bff
NickRoccodev11 <nchls.rcc@gmail.com>
2022-09-01 14:35:19 -0400

    daiy

--- 9.22/9.1.22.js
+++ 9.22/9.1.22.js
@@ -16,3 +16,20 @@ function factorial(n) {
   }
   return total === 0 ? 1 : total
 }
+
+// NEXT
+
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+function maskify(cc) {
+  cc = cc.split("");
+  let limit = cc.length - 4;
+  for (i = cc.length - 1; i >= 0; i--) {
+    if (i < limit) {
+      cc[i] = "#"
+    }
+  }
+  return cc.join("")
+}
