03d819c1662a5722f9e5f95d3e14015a58ae04ef
NickRoccodev11 <nchls.rcc@gmail.com>
2022-09-13 04:44:41 -0400

    refactored

--- 9.22/9.12.22.js
+++ 9.22/9.12.22.js
@@ -1,5 +1,7 @@
 
-// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+// Create a function that checks 
+// if a number n is divisible by two numbers x AND y.
+//  All inputs are positive, non-zero numbers.
 
 
 
