01af46aa4f655d4c437fc4adc18aea6dc29dff70
NickRoccodev11 <nchls.rcc@gmail.com>
2022-09-15 22:45:53 -0400

    changed answr

--- 6.22/6.3.22/sumsmallest.js
+++ 6.22/6.3.22/sumsmallest.js
@@ -3,19 +3,7 @@
 //  No floats or non-positive integers will be passed.
 
 
-// function sumTwoSmallestNumbers(numbers) {  
-//     return numbers.sort((a,b)=> a-b)[0]+ numbers.sort((a,b)=> a-b)[1]
-//     }
-
-function transpose(matrix) {
-let newArr = []
-for(i=0;i<matrix.length;i++){
-    newArr.push([])
-    for(j=0;j<matrix[i].length;j++){
-        newArr.push(matrix[i][j])
+function sumTwoSmallestNumbers(numbers) {  
+    return numbers.sort((a,b)=> a-b)[0]+ numbers.sort((a,b)=> a-b)[1]
     }
-}
-log(newArr)
-}
 
-transpose([[1,2,3],[4,5,6]])
