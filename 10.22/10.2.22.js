adfd8fd8152b397e21af99fa12234bf53370b13a
NickRoccodev11 <nchls.rcc@gmail.com>
2022-10-02 22:24:36 -0400

    added pedulum

--- 10.22/10.2.22.js
+++ 10.22/10.2.22.js
@@ -1,6 +1,8 @@
 
 // Sort the given array of strings in alphabetical order, case insensitive. For example:
 
+const { Form } = require("react-router-dom");
+
 // ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 // ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
 
@@ -14,4 +16,39 @@ function sortMe(names) {
   return names
   }
   
+
+
+// the pendulum! given an Array, return the lowest value in the center, 
+// with increasing values alternating From right and then left sides of center
+// example: [5,3,2,4,1]  => [5,3,1,2,4]
+
+
+
+
+
+  function pendulum(values) {
+    let pivot = Math.floor((values.length - 1) / 2)
+    let sortedArr = values.sort().slice();
+    let right = true;
+    log(pivot)
+    log(sortedArr)
+    log(values.length)
+   let counter = 1
+    for (i = 0; i < values.length; i++) {
+      if (right) {
+        sortedArr[ pivot + counter] = sortedArr[counter - 1]
+      } else {
+        sortedArr[pivot - counter] = sortedArr[counter + 1]
+      };
+     counter++
+      if (right) {
+        right = false
+      } else if (!right) {
+        right = true
+      }
+    }
+  return values
+  }
+  
+  log(pendulum([1, 5, 6, 3]))
   
--- OOP stopwatch/OOPstopwatch.js
+++ OOP stopwatch/OOPstopwatch.js
@@ -1,16 +1,29 @@
 let log = console.log
+////// DON'T DELETE
 
 
-let train = ["Hello", "my", "Name", "is", "Anne"]
-
-function sortMe(names) {
-  names.sort(function (a, b) {
-    return a.toLowerCase().localeCompare(b.toLowerCase());
-});
-return names
+function pendulum(values) {
+  let pivot = Math.floor((values.length - 1) / 2)
+  let sortedArr = values.sort().slice();
+  let right = true;
+  log(pivot)
+  log(sortedArr)
+  log(values.length)
+ let counter = 1
+  for (i = 0; i < values.length; i++) {
+    if (right) {
+      sortedArr[ pivot + counter] = sortedArr[counter - 1]
+    } else {
+      sortedArr[pivot - counter] = sortedArr[counter + 1]
+    };
+   counter++
+    if (right) {
+      right = false
+    } else if (!right) {
+      right = true
+    }
+  }
+return values
 }
 
-
-log(sortMe(train))
-
-
+log(pendulum([1, 5, 6, 3]))
