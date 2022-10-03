31053bae32a7e00c5e5e682aad3d4c96efab5103
NickRoccodev11 <nchls.rcc@gmail.com>
2022-10-03 12:37:04 -0400

    operations

--- OOP stopwatch/OOPstopwatch.js
+++ OOP stopwatch/OOPstopwatch.js
@@ -1,29 +1,25 @@
 let log = console.log
 ////// DON'T DELETE
 
+let stringy = "hello/nthere"
 
-function pendulum(values) {
-  let pivot = Math.floor((values.length - 1) / 2)
-  let sortedArr = values.sort().slice();
-  let right = true;
-  log(pivot)
-  log(sortedArr)
-  log(values.length)
- let counter = 1
-  for (i = 0; i < values.length; i++) {
-    if (right) {
-      sortedArr[ pivot + counter] = sortedArr[counter - 1]
-    } else {
-      sortedArr[pivot - counter] = sortedArr[counter + 1]
-    };
-   counter++
-    if (right) {
-      right = false
-    } else if (!right) {
-      right = true
+function horMirror(strng) {
+  let hResult='',
+    tempString = "";
+  for (i = 0; i < strng.length; i++) {
+    if (strng[i] !== "/") {
+      tempString += strng[i]
+      log(tempString)
+    } else if (strng[i] === "/") {
+      hResult += tempString.split('').reverse('').join('')
+      hResult += "/n"
+      tempString = ''
+      i++
+    }else if (i=== strng.length-1){
+      hResult += tempString.split('').reverse('').join('')
     }
+    log(hResult)
   }
-return values
 }
 
-log(pendulum([1, 5, 6, 3]))
+  horMirror(stringy)
