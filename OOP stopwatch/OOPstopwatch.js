209c9cfa8d5517d100166c07cc91d1c176518fe6
NickRoccodev11 <nchls.rcc@gmail.com>
2022-10-04 17:23:02 -0400

    unique array items

--- OOP stopwatch/OOPstopwatch.js
+++ OOP stopwatch/OOPstopwatch.js
@@ -1,25 +1,5 @@
 let log = console.log
 ////// DON'T DELETE
 
-let stringy = "hello/nthere"
 
-function horMirror(strng) {
-  let hResult='',
-    tempString = "";
-  for (i = 0; i < strng.length; i++) {
-    if (strng[i] !== "/") {
-      tempString += strng[i]
-      log(tempString)
-    } else if (strng[i] === "/") {
-      hResult += tempString.split('').reverse('').join('')
-      hResult += "/n"
-      tempString = ''
-      i++
-    }else if (i=== strng.length-1){
-      hResult += tempString.split('').reverse('').join('')
-    }
-    log(hResult)
-  }
-}
 
-  horMirror(stringy)
