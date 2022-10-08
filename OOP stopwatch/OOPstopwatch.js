70f41702074c75d0f6b194cb0b228240a0416ff6
NickRoccodev11 <nchls.rcc@gmail.com>
2022-10-08 13:47:44 -0400

    remove chars at nth index

--- OOP stopwatch/OOPstopwatch.js
+++ OOP stopwatch/OOPstopwatch.js
@@ -1,119 +1,14 @@
 let log = console.log
 ////// DON'T DELETE
 
-
-function catType(strng) {
-    let cleanedUp = ""
-
+function charRemover(strng, num) {
+    let cleanString = "";
     for (i = 0; i < strng.length; i++) {
-        if (strng[i] === "c") {
-            cleanedUp = strng.split("").slice(i + 1).join("")
-            break;
-        }
+        if ((i + 1) % num !== 0){
+            cleanString += strng[i]
+        } 
     }
-
-    let removeAndFlip = cleanedUp.replaceAll("Eek!", "").split('').reverse('').join('')
-
-    let splitByV = removeAndFlip.split('v')
-
-    let message = "";
-    for (i = 0; i < splitByV.length; i++) {
-        message += splitByV[i][0]
-    }
-
-    return message.slice(1)
+    return cleanString
 }
 
-log(catType("af)|Cz>:CGAHY1_2+(OHYYTM!2vka,&yAlWJ3.r&ZVs4F5&bEa<1r1_nlsR!eA-dOUmgwd2=CE7?ynnl+KQf8lW8u4/Nh1|D7SK2uYz-Y $YD)q.VmLD-&P(lL=:HDMA1!?_R#P,V3bKLqUp_t.X:_dM<Mr9zQ~fKJOBDF&Qjen=d ZAR8ze5g-gj@aZ/I:M:!.IBXLq@vN&8L7oH, /0RFtH+.7%eXtM/mxV7*%0Hj.?FSm.Kh8=@jlUJ_S.ApQzye-xB7ZVA2AwEaxTa_snPX?>D5th&Ag/9)7%#uMN=J<h!#prB=:CI;U_.T5*#rb)9q<ht~W<BOmQlmy.<8EqIlR(Gh~~$zmW;Y$7dn-$bW1jf%PL),krpzE LQwsG;EQ;l:Z;+6#.3)KDA+s#k#YMmUhm% 2KB)GqR U@4B>+A9Vh62@:&SNKVas5%&N6Oz!s/%7(Pd1xWy#Kv>uqyX=VEyHbe-64|ex^& 2W(gD=>FT*?tMZO$(+B8uTj,DU2~QST(B|xls=9kzN|:4hXt~:R4|nir,6RN8fg~3!3!^!yWZ)bE@SI.zV5Z%V9Qf_^ 4az$?wqtWrRu+1aS9<%I=!xW.:ps f).($Qz9s;8:uxVqJN9K$42NG(0=j)wZ&U1oB?v^7WVP6QE6-?L;^aglPd5dHO(L2TT5+$NeK-P;js-UAiWCI7>Yr&8|md-X)U=%IpuY)9iymv~ _S^ ,&0|(22&(mIcux_4a196w^FN78kz2kX1k&Psc53d ctbl?Eek!kX7Ii.QR~M11<T!,w^xHVn4^Q=HH4#)=AevEek!ux_4a196w^FN7Eek!8kz2kX1k&Psc53d ctbl?kX7Ii.QR~M11<T!,w^xHVn4^Q=HH4#)=AEek!Eek!cvEek!MABf|bEek!dPLpPhh=>%UH<%/^H;8gSAkhmUCu:K?*jzEek!LYC/:E*RU=|s7jWDf8z7-zFqS<Ntoav?m8<Q(1ur3Dpd)(XjY1pE)but>eG@QfEek!$Wx8NRZgmQ5t2 kJd,?_R@%muEJBTdQ0-%mHHW20i8wqMK~Co^w?34ag9idsTeXgfqy4IEek!z7Q?Q)nG~5@~ieY_B=6! sVk~=Ts3T>j/ZAS)AKX3zOocGL icK#-x0Eek!yE+2*fz.4&%<>:RHx/B+IFRG*AK1Hn*PQZpEek!v;ZUzWcJC%lym^:+;GcC!qt@nS5SQEek!ikESbYI#-A.Sv .ksat s'worrEek!Eek!omot Eek!rof rebmeEek!merEek! Eek!ot deeEek!n uoy tahw si enEek!o tsrif siht sedisEek!eb gnirtsbus hcae ni retEek!carahc tsrifEek! eEek!Eek!hT .'V' *esaEek!crewEek!ol* nEek!Eek!o pu Eek!gnirts Eek!siEek!ht tilpEek!S"))
-
-
-function replace(strng) {
-    let keyChars = ["S", "p", "a", "c", "e"],
-        newString = "",
-        portion = ""
-    for (i = 0; i < strng.length; i++) {
-        if (keyChars.includes(strng[i])) {
-            portion = strng.replace(strng[i], " ")
-            newString += portion.slice(newString.length, i + 1)
-        } else if (i === strng.length - 1) {
-            portion = portion.split(" ")
-            newString += portion[portion.length - 1]
-        }
-    }
-
-    return newString
-}
-
-log(replace("ZooSflupburnabun"))
-
-function stringReverse(strng) {
-    return strng.split('').reverse().join('')
-}
-
-// log(stringReverse("  Hello.! > "))
-
-let list = "The Pawshank Redemption,Caturday Night Live,Only Meworders in the Building,I Love Mewcy, Mewsummer Meowders,The Golden Purrs, Purrlandia ,Meowpardy, Meowstery Science Theater: Purrthousand, Amewican Idol,Dog City,Doctor Mew , The Meowing Fed,Mew Peter,The Vicar of Dogley, Kittens,Meownton Abbey,Pets and the Kitty,Dogis and Bonehead,Pawlty Meowers ,The Meowpet Show,Barkos,The Catbert Purrport,The Pawffice,The Dogford Files, Battlestar Catlactica,Catlumbo,SpongeDog Squarepants,NYPD Mew ,Fluffy the Meowpire Purrer,The Inbemewners,Meowder She Wrote,Paw & Order,30 Dog, Pawvatar: The Last Meowbender,The Pawnight Show,Arrested Dogvelopment,Furiends,Mewie,Curb Your Dogthusiasm,Teenage Mewtant Ninja Turtles,Phineas and Purrb,Paw Trek, Paw Trek: The Next Mewination, Twin Mewks, *C*A*T*S*,DogTales, Game of Bones, House of the Meowgon,The Purrlight Zone,Breaking Bone,The Meowre,The Dogpranos,The Rings of Meower, The KIT Crowd,Strangepaw Things ,Catman: The Animeowted Series,Meowter Call Saul,Mewgerton ,Obark,Mewphoria,La Casa de Pawpel,Rick & Meowty,Amewican Purror Story, Mewcifer,PawndaVision,Dogxter,The Meowndalorian, Dog Lasso,Bark,Meowdern Pawmily , Meowtlander,Bone Mirror,Barks and Recreation,How to Get Away with Meowder,Boneland ,Meowther Ted,Mewtopia,Mewey,The Mewkie Meowse Doghouse,Mewster Rogers' Neighborhood"
-
-function submissions(strng) {
-    let collection = strng.split(",")
-    let noWhiteSpace = collection.filter(el => el === el.trim())
-    let noDogs = []
-    for (i = 0; i < noWhiteSpace.length; i++) {
-        if (noWhiteSpace[i].toLowerCase().includes("dog") ||
-            noWhiteSpace[i].toLowerCase().includes("bark") ||
-            noWhiteSpace[i].toLowerCase().includes("bone")
-        ) {
-            false
-        } else {
-            noDogs.push(noWhiteSpace[i])
-        }
-    }
-    let noMultis = noDogs.filter(el => el.length % 5 !== 0)
-    let oddSum = [];
-    for (i = 0; i < noMultis.length; i++) {
-        if ((noMultis[i].charCodeAt(noMultis[i][0]) +
-            noMultis[i].charCodeAt(noMultis[i][noMultis[i].length - 1])) % 3 === 0) {
-            oddSum.push(noMultis[i])
-        }
-    }
-
-    let noEs = []
-    let discarded = []
-    for (i = 0; i < noMultis.length; i++) {
-        if (noMultis[i].length % 2 === 0) {
-            if (noMultis[i][Math.floor(noMultis[i].length / 2)] !== "e") {
-                noEs.push(noMultis[i])
-            } else if (noMultis[i].length % 3 === 0) {
-                if (noMultis[i][Math.floor(noMultis[i].length / 2 + 1)] !== "e") {
-                    noEs.push(noMultis[i])
-                }
-            }
-        }
-    }
-    let capCount = 0;
-    let twoCaps = []
-
-    for (i = 0; i < noEs.length; i++) {
-        for (j = 0; j < noEs[i].length; j++) {
-            if (noEs[i][j] === noEs[i][j].toUpperCase()) {
-                capCount++
-            }
-        }
-        if (capCount >= 2) {
-            twoCaps.push(noEs[i])
-            capCount = 0
-        }
-    }
-    
-    let noS = []
-    for(i=0;i< twoCaps.length; i++){
-        if(!twoCaps[i].includes("S")){
-            noS.push(twoCaps[i])
-        }
-    }
-   
-    return noS.length 
-}
-submissions(list)
-
-
+log(charRemover("The LBachyelor^ is ma tehrrib le tpelev!isioOn sh8ow.", 5))
