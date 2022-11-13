4522c73a5f96f19314c03ec6a1f479cbd3018074
NickRoccodev11 <nchls.rcc@gmail.com>
2022-11-12 23:19:34 -0500

    minimum steps required to reach finish

--- 11.22/11.12.22.js
+++ 11.22/11.12.22.js
@@ -4,7 +4,7 @@
 
 // You must write an algorithm with O(log n) runtime complexity.
 
- 
+
 
 // Example 1:
 
@@ -18,7 +18,7 @@
 
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
- 
+
 
 // Constraints:
 
@@ -32,7 +32,7 @@
  * @param {number} target
  * @return {number}
  */
- var searchInsert = function (nums, target) {
+var searchInsert = function (nums, target) {
     let lo = 0,
         hi = nums.length,
         mid;
@@ -53,8 +53,56 @@
 log(searchInsert([1, 3, 5, 6], 5)) // 2
 log(searchInsert([1, 3, 5, 6], 2)) // 1
 log(searchInsert([1, 3, 5, 6], 4)) // 2
-log(searchInsert([1, 3, 5, 6,7,8,9,10,15,17,18,20], 16)) // 9
-log(searchInsert([1, 3, 5, 6,9,12], 7)) // 4
+log(searchInsert([1, 3, 5, 6, 7, 8, 9, 10, 15, 17, 18, 20], 16)) // 9
+log(searchInsert([1, 3, 5, 6, 9, 12], 7)) // 4
 log(searchInsert([1, 3, 5, 6], 7)) // 4
 
+// An infinite number of shelves are arranged one above the other in a staggered fashion.
+// The cat can jump up to 3 shelves at the same time: from shelf 1 to shelf 2 or 4 (the cat cannot climb on the shelf directly above its head), according to the illustration:
 
+//                  ┌────────┐
+//                  │-6------│
+//                  └────────┘
+// ┌────────┐       
+// │------5-│        
+// └────────┘  ┌─────► OK!
+//             │    ┌────────┐
+//             │    │-4------│
+//             │    └────────┘
+// ┌────────┐  │
+// │------3-│  │     
+// BANG!────┘  ├─────► OK! 
+//   ▲  |\_/|  │    ┌────────┐
+//   │ ("^-^)  │    │-2------│
+//   │ )   (   │    └────────┘
+// ┌─┴─┴───┴┬──┘
+// │------1-│
+// └────────┘
+// Input
+// Start and finish shelf numbers (always positive integers, finish no smaller than start)
+
+// Task
+// Find the minimum number of jumps to go from start to finish
+
+// Example
+// Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+
+
+
+
+function solution(start, finish) {
+    let jumps = 0;
+    while (start < finish) {
+        if (finish - start >= 3) {
+            start += 3;
+            jumps++;
+        } else if (finish - start === 2) {
+            jumps += 2;
+            return jumps;
+        } else {
+            jumps++;
+            return jumps;
+        }
+    }
+    return jumps;
+}
