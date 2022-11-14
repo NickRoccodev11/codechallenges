50fc77fa05d7749776040529e9ca89ea04d465ae
NickRoccodev11 <nchls.rcc@gmail.com>
2022-11-13 23:07:26 -0500

    quadratic to constant time complexity

--- OOP stopwatch/OOPstopwatch.js
+++ OOP stopwatch/OOPstopwatch.js
@@ -11,12 +11,34 @@ let log = console.log
 
 // Pseudocode - talk about what you want to do HIGH LEVEL. then write out each step and look for possible snafus
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function findX(n) {
+    let x = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < 2 * n; j++) {
+
+            x += i+j
+            //   log(`i${i}   j${j}  x${x}`)
+        }
+    }
+   
+    // let j = (n * 2 - 1) * Math.pow(n, 2)
+   
+    // let i = (n - 1) * Math.pow(n,2)
+    return` ${x}   ${(n * 2 - 1) * Math.pow(n, 2) + (n - 1) * Math.pow(n,2)}`
+}
+
+// (n - 1) * n / 2 * n * 2
 
 
+78210
 
 
+// let x=2
+// log(x*x/2 + (x*2)*( x*2)/2)
+// 1 16 63 160
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
 // To calculate Big O, there are five steps you should follow:
 
 // Break your algorithm/function into individual operations
