a970c1cf3c6c6f432e569ec148e3c05496456e9b
NickRoccodev11 <nchls.rcc@gmail.com>
2022-11-16 06:23:47 -0500

    fixed comment

--- 11.22/11.16.22.js
+++ 11.22/11.16.22.js
@@ -1,4 +1,4 @@
-/ You need to create a function that when provided with a triplet, 
+//You need to create a function that when provided with a triplet, 
 // returns the index of the numerical element that lies between the other two elements.
 
 // The input to the function will be an array of three distinct numbers.
