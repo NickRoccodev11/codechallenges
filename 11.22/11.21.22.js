2909e539a08c195dce2d776b9f92514e31be83c2
NickRoccodev11 <nchls.rcc@gmail.com>
2022-11-21 10:49:20 -0500

    a function that recieves an array and a function, returns true if no elements in array return true from the func argument

---
+++ 11.22/11.21.22.js
@@ -0,0 +1,17 @@
+
+// Create a method none? (JS none) that accepts an array and a block (JS: a function), 
+// and returns true if the block (/function) returns true for none of the items in the array.
+//  An empty list should return true.
+
+
+function none(arr, fun){
+    if(arr.length == 0){
+      return true
+    }
+     for(i=0; i<arr.length; i++){
+       if(fun(arr[i])){
+         return false 
+       }
+     }
+     return true
+   }
