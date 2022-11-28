98d3a3a563129079eeffd7e599d6f950aefdfae4
NickRoccodev11 <nchls.rcc@gmail.com>
2022-11-28 17:05:42 -0500

    revised answer

--- 11.22/11.25.22.js
+++ 11.22/11.25.22.js
@@ -7,12 +7,13 @@
 // dup(["kelless","keenness"]) = ["keles","kenes"].
 // Strings will be lowercase only, no spaces 
 
-// dup(["ccooddddddewwwaaaaarrrrsssss","piccadilly","hubbubbubboo"]),['codewars','picaniny','hubububo'])
+// dup(["ccooddddddewwwaaaaarrrrsssss","piccadilly","hubbubbubboo"]),['codewars','picadilly','hubububo'])
 // dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese'])
 // dup(["kelless","keenness"]), ['keles','kenes'])
 
 
 const dup = (arr) =>  arr.map(string => string.split('').filter((letter, index, array) => letter !== array[index - 1]).join(''))
+
 log(dup(["abracadabra", "allottee", "assessee"]))
 log(dup(["kelless", "keenness"]))
 log(dup(["ccooddddddewwwaaaaarrrrsssss", "piccadilly", "hubbubbubboo"]))
