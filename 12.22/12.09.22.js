f29e32c7a63bcf9be9dafe08ed662a5d7d9ca004
NickRoccodev11 <nchls.rcc@gmail.com>
2022-12-09 14:14:52 -0500

    mine field

--- 12.22/12.09.22.js
+++ 12.22/12.09.22.js
@@ -1,16 +1,3 @@
-const log = console.log
-// ALWAYS DO PREP!!!   Parameters - Returns - Examples - Pseudocode
-
-// Parameters - clarify all possibilities for params. think of edge cases. 
-
-//Return - what is it exactly that you are returning? boolean? array? obj? string? mutated original / copy? 
-
-//Examples - talk through test cases, consider every outcome and log them with along with the answer 
-//  console.log(function(exampleA), 'true' ) //  console.log(function(exampleB), 'false' )
-
-// Pseudocode - talk about what you want to do HIGH LEVEL. then write out each step and look for possible snafus
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // DESCRIPTION:
 // Context
 // The Explosive Ordinance Disposal Unit of your country has found 
@@ -138,21 +125,3 @@ var field = [
     ['M', '.', '.', '.', 'T', '.', '.']
   ];
 log(safe_mine_field(field), [[0,1], [4,5] ] );
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-// To calculate Big O, there are five steps you should follow:
-
-// Break your algorithm/function into individual operations
-
-// Calculate the Big O of each operation
-
-// Add up the Big O of each operation together
-
-// Remove the constants
-
-// Find the highest order term — this will be what we consider the Big O 
-// of our algorithm/function
