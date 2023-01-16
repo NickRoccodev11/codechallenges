c3c0f932c8120d422ea18e18a47b0d9a5ef4aefd
NickRoccodev11 <nchls.rcc@gmail.com>
2023-01-15 20:40:24 -0500

    debugging exercise

--- 2023/1.23/1.15.23.js
+++ 2023/1.23/1.15.23.js
@@ -30,3 +30,30 @@ var lengthOfLongestSubstring = function (s) {
     }
     return longest > curSub.length ? longest : curSub.length
 };
+
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+// the loop:
+
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number;){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
+  // the fix: it had no expression step for the loop
+
+  function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
+
