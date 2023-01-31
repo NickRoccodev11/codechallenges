553cf54b0d97ec5d90b1273d5905dbcd73228941
NickRoccodev11 <nchls.rcc@gmail.com>
2023-01-31 14:07:06 -0500

    define property on array that counts number of occurences of an element

--- TESTCODE/test.js
+++ TESTCODE/test.js
@@ -11,85 +11,18 @@ const log = console.log;
 // Pseudocode - talk about what you want to do HIGH LEVEL. then write out each step and look for possible snafus
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var letterCombinations = function (digits) {
-
-    let phone = [[],
-    [],
-    ["a", "b", "c"],
-    ["d", "e", "f"],
-    ["g", "h", "i"],
-    ["j", "k", "l"],
-    ["m", "n", "o"],
-    ["p", "q", "r", "s"],
-    ["t", "u", "v"],
-    ["w", "x", "y", "z"]]
-
-    let words = [],
-        currW = '',
-        num1, num2, num3, num4;
-
-    if (digits.length === 0) {
-        return []
-    } else if (digits.length === 1) {
-        num1 = parseInt(digits)
-        return phone[num1]
-    } else if (digits.length === 2) {
-        num1 = parseInt(digits[0])
-        num2 = parseInt(digits[1])
-
-        for (i = 0; i < phone[num1].length; i++) {
-            for (j = 0; j < phone[num2].length; j++) {
-                currW += phone[num1][i] + phone[num2][j]
-                words.push(currW)
-                currW = ''
-            }
-        }
-        return words
-    } else if (digits.length === 3) {
-        num1 = parseInt(digits[0])
-        num2 = parseInt(digits[1])
-        num3 = parseInt(digits[2])
-        for (i = 0; i < phone[num1].length; i++) {
-            for (j = 0; j < phone[num2].length; j++) {
-                for (k = 0; k < phone[num3].length; k++) {
-                    currW += phone[num1][i] + phone[num2][j] + phone[num3][k]
-                    words.push(currW)
-                    currW = ''
-                }
-            }
-        }
-        return words
-    } else if (digits.length === 4) {
-        num1 = parseInt(digits[0])
-        num2 = parseInt(digits[1])
-        num3 = parseInt(digits[2])
-        num4 = parseInt(digits[3])
-
-        for (i = 0; i < phone[num1].length; i++) {
-            for (j = 0; j < phone[num2].length; j++) {
-                for (k = 0; k < phone[num3].length; k++) {
-                    for (l = 0; l < phone[num4].length; l++) {
-                        currW += phone[num1][i] + phone[num2][j] + phone[num3][k] + phone[num4][l]
-                        words.push(currW)
-                        currW = ''
-                    }
-
-                }
-            }
-        }
-        return words
+Object.defineProperty(Array.prototype, 'numberOfOccurrences',{ 
+    value : function numberOfOccurrences(element) {
+      let occurrences = 0
+      this.forEach((el)=> el === element ? occurrences++ : null )
+      return occurrences
     }
+  });
 
-};
+let arr = [1,2,3,4,5,6,7,7,7,7,7,2]
 
-// log(letterCombinations(""))
-// log(letterCombinations("9"))
-log(letterCombinations("7979"))
-
-
-
-
-
+log(arr.numberOfOccurrences(2))
+log(arr.numberOfOccurrences(7))
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // To calculate Big O, there are five steps you should follow:
 
