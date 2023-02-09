c5304f2a36054f8cfc7afebd1cb0e03ec58898e7
NickRoccodev11 <nchls.rcc@gmail.com>
2023-02-09 11:11:00 -0500

    singly linked list

--- 2023/2.23/2.9.23.js
+++ 2023/2.23/2.9.23.js
@@ -1,16 +1,4 @@
-const log = console.log;
-// ALWAYS DO PREP!!!   Parameters - Returns - Examples - Pseudocode
-
-// Parameters - clarify all possibilities for params. think of edge cases. 
-
-//Return - what is it exactly that you are returning? boolean? array? obj? string? mutated original / copy? 
-
-//Examples - talk through test cases, cosider every outcome and log them with along with the answer 
-//  console.log(function(exampleA), 'true' ) //  console.log(function(exampleB), 'false' )
-
-// Pseudocode - talk about what you want to do HIGH LEVEL. then write out each step and look for possible snafus
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//singly linked list
 class Node {
     constructor(data, next = null) {
         this.data = data
@@ -145,18 +133,4 @@ ll.printListData()
 ll.insertAt(3,2)
 ll.printListData()
 ll.insertFirst(0)
-ll.printListData()
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-// To calculate Big O, there are five steps you should follow:
-
-// Break your algorithm/function into individual operations
-
-// Calculate the Big O of each operation
-
-// Add up the Big O of each operation together
-
-// Remove the constants
-
-// Find the highest order term — this will be what we consider the Big O 
-// of our algorithm/function
+ll.printListData
