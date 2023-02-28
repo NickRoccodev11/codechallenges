f406dc8c46bd15da50e3b5b5a3adb5ad24a1869c
NickRoccodev11 <nchls.rcc@gmail.com>
2023-02-28 14:18:34 -0500

    add length to a string

---
+++ 2023/1.23/1.28.23.js
@@ -0,0 +1,11 @@
+//turn a string into an array that contains the length of each
+//  word in the string separated by a space
+// "apple ban"  =>   ["apple 5", "ban 3"]
+
+
+function addLength(str) {
+    let strArr= str.split(" "),
+      answer= [];
+      strArr.forEach((el)=> answer.push(`${el} ${el.length}`))
+      return answer
+    }
