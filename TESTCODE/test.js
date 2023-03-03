fe0ca18dcbf7e4ef1366473066a78e37a410dcbe
NickRoccodev11 <nchls.rcc@gmail.com>
2023-03-03 11:21:48 -0500

    backspace compare

--- TESTCODE/test.js
+++ TESTCODE/test.js
@@ -11,160 +11,40 @@ const log = console.log;
 // Pseudocode - talk about what you want to do HIGH LEVEL. then write out each step and look for possible snafus
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    let textS = []
+    let textT = []
 
-
-
-
-function reverseList(head){
-    let prev= null;
-    let next = null;
-
-    while(head!== null){
-        next = head.next;
-        head.next = prev;
-        prev = head;
-        head = next;
-    }
-    return prev
-}
-
-
-
-
-
-//singly linked list
-class Node {
-    constructor(data, next = null) {
-        this.data = data
-        this.next = next
-    }
-}
-
-
-class LinkedListSingle {
-    constructor() {
-        this.head = null
-        this.size = 0
-    }
-
-    //insert first node
-    insertFirst(data) {
-        this.head = new Node(data, this.head);
-        this.size++
-    }
-
-    //insert last node
-    insertLast(data) {
-        let node = new Node(data)
-        let current
-
-        //if empty, make head
-        if (!this.head) {
-            this.head = node
-        } else {
-            current = this.head
-            while (current.next) {
-                current = current.next
-            }
-            current.next = node
-        }
-        this.size++
-    }
-
-    //insert at specific index
-    insertAt(data, index) {
-        //if idx is out of range
-        if (index > 0 && index > this.size) {
-            return
-        }
-
-        //if inserting at first idx
-        if (index === 0) {
-            this.insertFirst(data);
-            return;
-        }
-
-        const node = new Node(data)
-        let current, previous;
-
-        //set current to first
-        current = this.head;
-        let count = 0
-
-        while (count < index) {
-            previous = current; //node b4 idx
-            count++
-            current = current.next //node after idx
-        }
-        node.next = current
-        previous.next = node
-        this.size++
-    }
-    //log data from a requested idx
-    getAtIndex(index) {
-        let current = this.head;
-        let count = 0;
-        while (current) {
-            if (count === index) {
-                console.log(current.data)
-            }
-            count++
-            current = current.next
-        }
-        return null
-    }
-
-    removeAtIndex(index) {
-        if (index > 0 && index > this.size) {
-            return
-        }
-
-        let current = this.head
-        let previous;
-        let count = 0
-
-        //Remove first 
-        if (index == 0) {
-            this.head = current.next
-        } else {
-            while (count < index) {
-                count++
-                previous = current
-                current = current.next
-            }
-            previous.next = current.next
-        }
-        this.size--
-    }
-
-
-    // Clear list
-    clearList() {
-        this.head = null;
-        this.size = 0;
-    }
-
-    printListData() {
-        let current = this.head
-        while (current) {
-            console.log(current.data)
-            current = current.next
+    for(var i = 0; i < s.length; i++){
+        if(s[i] === "#" && textS.length > 0 ){
+            textS.pop()
+        }else if (s[i] !== "#" ){
+            textS.push(s[i])
         }
     }
+    for(var i = 0; i < t.length; i++){
+        if(t[i] === "#" && textT.length > 0 ){
+            textT.pop()
+            log(`popped: textT = ${textT}`)
+        }
+        if (t[i] !== "#" ){
+            log(`i= ${i} letter: ${t[i]}`)
+            textT.push(t[i])
+            log(` post push: ${textT} i= ${i}`)
+        }
+    }
+// log(textS)
+log(textT)
+return textS.join("") === textT.join("")
 
-}
+};
 
-
-let ll = new LinkedListSingle;
-ll.insertFirst(1)
-ll.insertLast(2)
-ll.insertLast(3)
-ll.insertLast(4)
-ll.insertFirst(0)
-
-console.log(ll)
-console.log(reverseList(ll))
-
+log(backspaceCompare("rmp","rmu#p"))
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // To calculate Big O, there are five steps you should follow:
 
