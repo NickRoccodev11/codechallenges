d9561c11e785d86134b6ff9c96b32ee4104d90cf
NickRoccodev11 <nchls.rcc@gmail.com>
2023-03-13 12:59:09 -0400

    hello world

---
+++ 2023/3.23/3.13.23.js
@@ -0,0 +1,18 @@
+Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+const greet = () => {
+    let h = "h"
+    let e = "e"
+    let l = "l"
+    let o = "o"
+    let space = " "
+    let w = "w"
+    let r = "r"
+    let d = "d"
+    let i = "!"
+    return h+e+l+l+o+space+w+o+r+l+d+i
+  }
