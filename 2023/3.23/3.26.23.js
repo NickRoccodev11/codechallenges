bfd9b3a4d339efc783722ad51da1c01ea2b356ff
NickRoccodev11 <nchls.rcc@gmail.com>
2023-03-25 11:10:06 -0400

    character to ascii returned as a has table

---
+++ 2023/3.23/3.26.23.js
@@ -0,0 +1,15 @@
+// Take a string and return a hash with all the ascii values of the characters in the string. Returns nil if the string is empty. The key is the character, and the value is the ascii value of the character. 
+// Repeated characters are to be ignored and non-alphebetic characters as well.
+
+function charToAscii(string) {
+    if (string.length === 0) {
+        return null
+    }
+    let hash = {}
+    for (char of string) {
+        if (!hash[char] && char.toUpperCase() !== char.toLowerCase()) {
+            hash[char] =  string.charCodeAt(string.indexOf(char))
+        }
+    }
+    return hash
+}
