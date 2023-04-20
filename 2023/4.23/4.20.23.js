b75cc0db22cfa35c5d69ba5cae7825a47f645e05
NickRoccodev11 <nchls.rcc@gmail.com>
2023-04-20 11:25:50 -0400

    space removal

---
+++ 2023/4.23/4.20.23.js
@@ -0,0 +1,13 @@
+// evin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
+// For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+
+
+function spacey(array){
+    let response = []
+    let wordBall = ''
+    for (i=0;i<array.length; i++){
+      wordBall += array[i];
+      response.push(wordBall)
+    }
+      return response
+    }
