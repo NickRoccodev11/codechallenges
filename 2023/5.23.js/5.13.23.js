c68df68c4b3ec0e5bcf39eb7b6f5ecfb4daf164d
NickRoccodev11 <nchls.rcc@gmail.com>
2023-05-15 11:54:33 -0400

    twoSum second pass, not finished

--- 2023/5.23.js/5.13.23.js
+++ 2023/5.23.js/5.13.23.js
@@ -23,27 +23,54 @@ var twoSum = function (nums, target) {
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let numMap= {}
-    for (n of nums){
-        if(!numMap[n]){
+var twoSum = function (nums, target) {
+    let numMap = {}
+    for (n of nums) {
+        if (!numMap[n]) {
             numMap[n] = n
         }
     }
-  log(numMap)
-  log(nums, "nums")
-  log(nums.indexOf(2))
+    log(numMap)
+    log(nums, "nums")
+    let num = 2
+    log("WHAT")
 
-    for(num in numMap){
+    for (num in numMap) {
         log(num, "top")
         log(target - num, "t-n")
-        if(nums.includes(target - num)){
-        log("match", num, nums.indexOf(target-num))
-        log(num, "and...")
-        log(nums.indexOf(num))
-        log(nums)
-          return [nums.indexOf(target - num), nums.indexOf(num)]
+        if (nums.includes(target - num)) {
+            log("match", num, nums.indexOf(target - num))
+            log(num, "and...")
+            log(nums.indexOf(num))
+            log(nums)
+            return [nums.indexOf(target - num), nums.indexOf(num)]
         }
     }
-  };
-log(twoSum([2,7,11,15],9))
+};
+log(twoSum([2, 7, 11, 15], 9))
+
+
+// PROBLEM FOUND: Object keys are always strings! you are trying to return the string "2" instead of the int
+
+// try three:
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let map = {};
+    for (i = 0; i < nums.length; i++) {
+        map[i] = nums[i]
+    }
+    for (idx in map) {
+        if (nums.includes(target - map[idx])) {
+            let idx1 = parseInt(idx)
+            let idx2 = nums.indexOf(target - map[idx])
+            if (idx1 !== idx2) {
+                return [idx1, idx2]
+            }
+        }
+    }
+
+};
--- TESTCODE/test.js
+++ TESTCODE/test.js
@@ -15,30 +15,23 @@ const log = console.log;
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let numMap= {}
-    for (n of nums){
-        if(!numMap[n]){
-            numMap[n] = n
-        }
+var twoSum = function (nums, target) {
+let map= {};
+for(i=0;i<nums.length; i++){
+    map[i] = nums[i]
+}
+for(idx in map){
+    if(nums.includes(target - map[idx] ) ){
+      let idx1 = parseInt(idx)
+      let idx2= nums.indexOf(target-map[idx])  
+      if(idx1!== idx2){
+        return [idx1, idx2]
+      }
     }
-  log(numMap)
-  log(nums, "nums")
-  log(nums.indexOf(2))
+}
 
-    for(num in numMap){
-        log(num, "top")
-        log(target - num, "t-n")
-        if(nums.includes(target - num)){
-        log("match", num, nums.indexOf(target-num))
-        log(num, "and...")
-        log(nums.indexOf(num))
-        log(nums)
-          return [nums.indexOf(target - num), nums.indexOf(num)]
-        }
-    }
-  };
-log(twoSum([2,7,11,15],9))
+};
+log(twoSum([3, 4, 5, 3], 6))
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // To calculate Big O, there are five steps you should follow:
