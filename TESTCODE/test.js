5e6cf87cbbc1866fb0c18fa12e454aacb3974887
NickRoccodev11 <nchls.rcc@gmail.com>
2023-06-08 16:44:16 -0400

    day 2, recursive solution to digitsum

--- TESTCODE/test.js
+++ TESTCODE/test.js
@@ -15,15 +15,30 @@ const log = console.log;
 
 
 //determine length of string and if its greater than k
-//
+//push sliced chunks into new array
+// convert to numbers, sum them, and then join it all back to a string to check for length
 
 
-var digitSum = function(s, k) {
-  let arr =[]
-    for(i=0; i<s.length; i+=k){
-      arr.push(s.slice(i,i+k))
+var digitSum = function (s, k) {
+  if (s.length <= k) {
+    return s
+  } else {
+    let arr = []
+    for (i = 0; i < s.length; i += k) {
+      arr.push(s.slice(i, i + k))
     }
-    console.log(arr)
+    let sum = 0
+    let sumArr = []
+    for (i = 0; i < arr.length; i++) {
+      for (j = 0; j < arr[i].length; j++) {
+        sum += Number(arr[i][j])
+      }
+      sumArr.push(sum)
+      sum = 0;
+    }
+    s = sumArr.join("")
+  }
+  return digitSum(s,k)
 };
 
 console.log(digitSum("11111222223", 3))
