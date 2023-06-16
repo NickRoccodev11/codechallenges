bfff37820db7e132e678f8dfd8d4796e08961dcd
NickRoccodev11 <nchls.rcc@gmail.com>
2023-06-16 16:28:44 -0400

    122 tests passed, some edge cases remain

--- TESTCODE/test.js
+++ TESTCODE/test.js
@@ -13,3 +13,40 @@ const log = console.log;
 //5 Find the highest order term — this will be what we consider the Big O of our algorithm/function
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+// The k-beauty of an integer num is defined as the number of substrings of num when it is read as a string that meet the following conditions:
+
+// It has a length of k.
+// It is a divisor of num.
+// Given integers num and k, return the k-beauty of num.
+
+// Note:
+
+// Leading zeros are allowed.
+// 0 is not a divisor of any value.
+// A substring is a contiguous sequence of characters in a string.
+
+//  MY PLAN:
+
+// save num as a string
+// traverse the string by steps of k
+// with each step, reassign a current variable which acts as divisor
+//test it against num
+// if it passes, increment a "K-beauty" variable
+// return kbeauty
+
+var divisorSubstrings = function (num, k) {
+  let sNum = num.toString();
+  let current
+  let kBeauty = 0
+  for (i = 0; i < sNum.length; i++) {
+    curr = Number(sNum.slice(i, i + k))
+    if (num % curr === 0 && curr !== 0) {
+      kBeauty++
+    }
+  }
+  return kBeauty
+};
+
+console.log(divisorSubstrings(430043, 2))
+console.log(divisorSubstrings(240,2))
