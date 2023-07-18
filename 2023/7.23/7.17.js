8ed8b4b7ea27ac50797a76e72d0b6bc14481a988
NickRoccodev11 <nchls.rcc@gmail.com>
2023-07-18 16:57:16 -0400

    find a common substring

---
+++ 2023/7.23/7.17.js
@@ -0,0 +1,15 @@
+// Color Ghost
+// Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+class Ghost{
+    constructor(){
+      let colors =["red","purple","white", "yellow"]
+      this.color = colors[Math.floor(Math.random()*4)]
+    }
+  }
+let ghosty = new Ghost();
+  console.log(ghosty.color)
--- 2023/7.23/7.18.js
+++ 2023/7.23/7.18.js
@@ -1,6 +1,3 @@
-let log = console.log
-/////////////////////////////////////////////////////////////////////////
-
 // Given 2 strings, your job is to find out if there is a substring that appears in both strings. You will return true if you find a substring that appears in both strings, or false if you do not. We only care about substrings that are longer than one letter long.
 
 // #Examples:
@@ -12,13 +9,6 @@ let log = console.log
 // SubstringTest("Something","Home"); //Returns true
 // In the above example, example 2 returns true because both of the inputs contain the substring "me". (soMEthing and hoME)
 // In example 1, the method will return false because something and fun contain no common substrings. (We do not count the 'n' as a substring in this Kata because it is only 1 character long)
-
-// #Rules: Lowercase and uppercase letters are the same. So 'A' == 'a'.
-// We only count substrings that are > 1 in length.
-
-// #Input: Two strings with both lower and upper cases. #Output: A boolean value determining if there is a common substring between the two inputs.
-
-
 function SubstringTest(str1, str2) {
     for (i = 0; i < str1.length; i++) {
         for (j = 0; j < str2.length; j++) {
