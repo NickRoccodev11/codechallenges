7f9c7c8505b6f952e2c49f61467c5868f752dfff
NickRoccodev11 <nchls.rcc@gmail.com>
2023-07-19 17:16:03 -0400

    substring method

--- 2023/7.23/7.19.js
+++ 2023/7.23/7.19.js
@@ -1,5 +1,5 @@
-let log = console.log
-/////////////////////////////////////////////////////////////////////////
+//  SUBSTRING METHOD
+
 
 let stringy = "abcdefghijklmnopqrstuvwxyz"
                   
@@ -8,4 +8,4 @@ console.log(stringy.substring(2, 10))//cdefghij   => notice index 10 is not incl
 console.log(stringy.substring(2, 0))//ab   => notice we only get indeces 0 and 1
 console.log(stringy.substring(4,1))//bcd => If start is greater than end, arguments are swapped: (4, 1) = (1, 4).
 console.log(stringy.substring(-1,1))//a  => numbers less than zero are treated as zero
-
+console.log(stringy.substring(0))//abcdefghijklmnopqrstuvwxyz => no second arg means go to end
