58575bf715adabc3fa6a722013faf5800f5de614
NickRoccodev11 <nchls.rcc@gmail.com>
2023-10-18 12:17:55 -0400

    switch case

--- TESTCODE/test.js
+++ TESTCODE/test.js
@@ -18,27 +18,10 @@ const log = console.log;
 //push sliced chunks into new array
 // convert to numbers, sum them, and then join it all back to a string to check for length
 
+function VarNamer(){
+  var namer = "nicholas"
+  return namer
+}
 
-var digitSum = function (s, k) {
-  if (s.length <= k) {
-    return s
-  } else {
-    let arr = []
-    for (i = 0; i < s.length; i += k) {
-      arr.push(s.slice(i, i + k))
-    }
-    let sum = 0
-    let sumArr = []
-    for (i = 0; i < arr.length; i++) {
-      for (j = 0; j < arr[i].length; j++) {
-        sum += Number(arr[i][j])
-      }
-      sumArr.push(sum)
-      sum = 0;
-    }
-    s = sumArr.join("")
-  }
-  return digitSum(s,k)
-};
-
-console.log(digitSum("11111222223", 3))
+VarNamer()
+console.log(namer)
