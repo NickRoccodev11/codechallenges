ced5d5be911d924c61e1c0e4470d77d52deada2b
NickRoccodev11 <nchls.rcc@gmail.com>
2023-11-03 23:49:33 -0400

    dystopian scenario

--- TESTCODE/test.js
+++ TESTCODE/test.js
@@ -1,11 +1,15 @@
 let log = console.log
 /////////////////////////////////////////////////////////////////////////
 
-let stringy = "abcdefghijklmnopqrstuvwxyz"
-                  
+function match(usefulness, months) {
+    let useScore = usefulness.reduce((a, b) => a + b, 0)
+    let needs = 100;
+    for (i = 1; i <= months; i++) {
+        let reduction = (needs * 15) / 100;
+        needs -= reduction
+    }
 
-console.log(stringy.substring(2, 10))//cdefghij   => notice index 10 is not included
-console.log(stringy.substring(2, 0))//ab   => notice we only get indeces 0 and 1
-console.log(stringy.substring(4,1))//bcd => If start is greater than end, arguments are swapped: (4, 1) = (1, 4).
-console.log(stringy.substring(-1,1))//a  => numbers less than zero are treated as zero
+    return useScore >= needs ? "Match!" : "No match!"
+}
 
+console.log(match([15, 24, 12], 4))
