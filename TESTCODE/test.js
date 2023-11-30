970428687088734f4ab5da1b75427d2c1bd4026d
NickRoccodev11 <nchls.rcc@gmail.com>
2023-11-30 14:13:18 -0500

    unique letters in a string

--- TESTCODE/test.js
+++ TESTCODE/test.js
@@ -1,30 +1,13 @@
 let log = console.log
 /////////////////////////////////////////////////////////////////////////
-// you are given a 0-indexed integer array nums and an integer k. Your task is to perform the following operation exactly k times in order to maximize your score:
-
-// Select an element m from nums.
-// Remove the selected element m from the array.
-// Add a new element with a value of m + 1 to the array.
-// Increase your score by m.
-// Return the maximum score you can achieve after performing the operation exactly k times.
-
-// run a math.max on nums to find the largest num
-//create a sum var
-// loop over largest, iterate k times
-//each time, add largest to sum, then largest++
-//return sum
-
 /**
- * @param {number[]} nums
- * @param {number} k
+ * @param {string} s
  * @return {number}
  */
-var maximizeSum = function (nums, k) {
-    let largest = Math.max(...nums)
-    let sum = 0;
-    for (var i = 1; i <= k; i++) {
-        sum += largest;
-        largest++;
-    }
-    return sum
+var minimizedStringLength = function (s) {
+    let sArr = s.split("")
+    let final = sArr.filter((e, i, a) => a.indexOf(e) === a.lastIndexOf(e))
+    return final.length
 };
+
+log(minimizedStringLength("hello"))
