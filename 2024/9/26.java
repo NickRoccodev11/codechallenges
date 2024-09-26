44dc6ff7da8765dbdaa9ec6c483f44eba1719014
NickRoccodev11 <nchls.rcc@gmail.com>
2024-09-26 16:21:05 -0400

    add the two smallest numbers in the array, in java

---
+++ 2024/9/26.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Kata{
+  public static long sumTwoSmallestNumbers(final long [] numbers) {
+    long lowestSum = 0;
+    
+   Long smallest = Long.MAX_VALUE;
+   Long secondSmallest = Long.MAX_VALUE;
+    
+    for(int i = 0; i < numbers.length; i++){
+      if(numbers[i] < smallest ){
+        secondSmallest = smallest;
+        smallest = numbers[i];
+      }else if (numbers[i] < secondSmallest){
+        secondSmallest = numbers[i];
+      }
+    }
+    
+    return smallest + secondSmallest; 
+  }             
+}
